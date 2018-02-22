9289f175ccbd98c16645d6640505a540de6f71f8
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2018-02-22 14:08:10 -0700

    changed name convention to other phenix executables

---
+++ gromacs-4.5.5_cryo_fit/src/gmxlib/version.c
@@ -0,0 +1,4 @@
+#include "version.h"
+const char _gmx_ver_string[] = "VERSION 4.5.5-20110921-c9635ce-dirty";
+const char _gmx_full_git_hash[] = "c9635cec6fd784301000b4330022a227c3a6da2a-dirty";
+const char _gmx_central_base_hash[] = "";
